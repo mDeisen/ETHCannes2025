2edca0d34654cf1f3c020927c263c83232bbfac3
Maxim <maxim3631@gmail.com>
2025-07-05 23:15:26 +0200

    Add schema

---
+++ webapp/src/connectors/schema.ts
@@ -0,0 +1,22 @@
+import { Entity, Type } from '@graphprotocol/hypergraph';
+
+export class User extends Entity.Class<User>('User')({
+    name: Type.Text,
+  }) {}
+  
+  export class Group extends Entity.Class<Group>('Group')({
+    name: Type.Text,
+    users: Type.Relation(User),
+  }) {}
+  
+  export class Application extends Entity.Class<Application>('Application')({
+    domain: Type.Text,
+    name: Type.Text,
+  }) {}
+  
+  export class Permission extends Entity.Class<Permission>('Permission')({
+    name: Type.Text,
+    application: Type.Relation(Application),
+    group: Type.Relation(Group),
+    permissionType: Type.Text,
+  }) {}
