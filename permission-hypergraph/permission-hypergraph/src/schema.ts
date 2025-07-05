94b8a95db573c46299dcde97068aedddf912e655
Maxim <maxim3631@gmail.com>
2025-07-05 15:07:51 +0200

    Enhance schema

--- permission-hypergraph/permission-hypergraph/src/schema.ts
+++ permission-hypergraph/permission-hypergraph/src/schema.ts
@@ -1,18 +1,21 @@
 import { Entity, Type } from '@graphprotocol/hypergraph';
 
+export class User extends Entity.Class<User>('User')({
+  wallet: Type.Text,
+}) {}
+
+export class Group extends Entity.Class<Group>('Group')({
+  createdAt: Type.Date,
+  users: Type.Relation(User),
+}) {}
+
 export class Application extends Entity.Class<Application>('Application')({
   domain: Type.Text
 }) {}
 
 export class Permission extends Entity.Class<Permission>('Permission')({
-  givenAt: Type.Date
-}) {}
-
-export class Group extends Entity.Class<Group>('Group')({
-  createdAt: Type.Date
-}) {}
-
-export class User extends Entity.Class<User>('User')({
-  wallet: Type.Text,
-  createdAt: Type.Date
+  givenAt: Type.Date,
+  application: Type.Relation(Application),
+  group: Type.Relation(Group),
+  type: Type.Text,
 }) {}
